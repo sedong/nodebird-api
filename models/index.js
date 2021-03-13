95d3ac7d305ad3fa527eaeea03cace2aa278b39d
sdcheon <sdcheon@lfpartners.co.kr>
2021-03-13 12:02:10 +0900

    api 테스트

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const config = require(__dirname + '/../config/config.json')[env];
 const User = require('./user'); //모델
 const Post = require('./post'); //모델
 const Hashtag = require('./hashtag'); //모델
+const Domain = require('./domain'); //모델
 
 const db = {};
 const sequelize = new Sequelize(config.database,config.username,config.password,config,);
@@ -12,13 +13,16 @@ db.sequelize = sequelize;
 db.User = User;
 db.Post = Post;
 db.Hashtag = Hashtag;
+db.Domain = Domain;
 
 User.init(sequelize);
 Post.init(sequelize);
 Hashtag.init(sequelize);
+Domain.init(sequelize);
 
 User.associate(db);
 Post.associate(db);
 Hashtag.associate(db);
+Domain.associate(db);
 
 module.exports = db;
